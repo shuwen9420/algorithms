4999796bfb3833d9cedd160734b6965d97a70721
shuwen0420 <20150201018@m.scnu.edu.cn>
2021-06-07 14:56:26 +0800

    addfile

--- src/main/java/algorithm_practice/nowcoder/a_1st_Season/aa_Manacher_bfprt_KMP/KMP.java
+++ src/main/java/algorithm_practice/nowcoder/a_1st_Season/aa_Manacher_bfprt_KMP/KMP.java
@@ -23,8 +23,6 @@ public class KMP {
         int ps2 = 0;    //遍历str2用的指针
         //djfdjsgfjshgjshgjshj
         //well
-        //dfd
-        //hello
         int[] nextArr = getNextArray(s2);
         while (ps1 < s1.length && ps2 < s2.length) {
             if (s1[ps1] == s2[ps2]) {

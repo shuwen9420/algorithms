34031be95462e0aab725aae5796f8289dd257940
shuwen0420 <20150201018@m.scnu.edu.cn>
2021-06-07 14:24:58 +0800

    addfile

---
+++ src/main/java/algorithm_practice/nowcoder/a_1st_Season/aa_Manacher_bfprt_KMP/Ominios.java
@@ -0,0 +1,7 @@
+package algorithm_practice.nowcoder.a_1st_Season.aa_Manacher_bfprt_KMP;
+
+public class Ominios {
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
